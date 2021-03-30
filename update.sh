12951cbb13823e6e89e4e9616e02b52cc4860d91
luyiwind <70989874+luyiwind@users.noreply.github.com>
2021-03-30 09:21:24 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,3 +3,7 @@ cd /root/go/src/DownloadBot
 git fetch --all
 git reset --hard origin/main
 git pull
+go build 
+systemctl stop aria2bot.service
+cp DownloadBot /root/aria2Bot/DownloadBot
+systemctl start aria2bot.service
